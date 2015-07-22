a651fe64fb2ab6db81cff7460300f5e793d2f77b
relberger <rich@rae-consulting.com>
2015-07-22 16:48:36 -0400

    update changes so far to next dev release and version bump Path.pm.

--- lib/File/Path.pm
+++ lib/File/Path.pm
@@ -18,7 +18,7 @@ BEGIN {
 
 use Exporter ();
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-$VERSION   = '2.11';
+$VERSION   = '2.11_001';
 $VERSION   = eval $VERSION;
 @ISA       = qw(Exporter);
 @EXPORT    = qw(mkpath rmtree);
