ada80fac0c6589162f15af5df005522cad628db0
Graham Knop <haarg@haarg.org>
2015-07-24 17:44:48 -0400

    5.6 compatibility in tests

--- t/FilePathTest.pm
+++ t/FilePathTest.pm
@@ -25,13 +25,41 @@ sub _run_for_warning {
 
 sub _run_for_verbose {
   my $coderef = shift;
-  my $stdout;
+  my $stdout = '';
   {
-    open my $stdout_fh, '>', \$stdout;
-    my $guard = SelectSaver->new($stdout_fh);
+    my $guard = SelectSaver->new(_ref_to_fh(\$stdout));
     &$coderef;
   }
   return $stdout;
+}
+
+sub _ref_to_fh {
+  my $output = shift;
+  open my $fh, '>', $output;
+  return $fh;
+}
+
+BEGIN {
+  if ($] < 5.008000) {
+    eval qq{#line @{[__LINE__+1]} "@{[__FILE__]}"\n} . <<'END' or die $@;
+      no warnings 'redefine';
+      use Symbol ();
+
+      sub _ref_to_fh {
+        my $output = shift;
+        my $fh = Symbol::gensym();
+        tie *$fh, 'StringIO', $output;
+        return $fh;
+      }
+
+      package StringIO;
+      sub TIEHANDLE { bless [ $_[1] ], $_[0] }
+      sub CLOSE    { @{$_[0]} = (); 1 }
+      sub PRINT    { ${ $_[0][0] } .= $_[1] }
+      sub PRINTF   { ${ $_[0][0] } .= sprintf $_[1], @_[2..$#_] }
+      1;
+END
+  }
 }
 
 1;
